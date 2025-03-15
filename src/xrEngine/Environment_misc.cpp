OpenXRay/xray-16

commit f9543167cb4666471d45a7fa2b905cd6d128e0d4

Weather editor improvements: Reworked SOC style weather mode General day time slider and a slider between two time frames Many other changes

src/xrEngine/Environment_misc.cpp
@@ -251,7 +251,6 @@ void CEnvAmbient::load(
 //-----------------------------------------------------------------------------
 CEnvDescriptor::CEnvDescriptor(shared_str const& identifier) : m_identifier(identifier)
 {
-    old_style = false;
     dont_save = false;
 
     exec_time = 0.0f;
@@ -281,6 +280,7 @@ CEnvDescriptor::CEnvDescriptor(shared_str const& identifier) : m_identifier(iden
     hemi_color.set(1, 1, 1, 1);
     sun_color.set(1, 1, 1);
     sun_dir.set(0, -1, 0);
+    use_dynamic_sun_dir = true;
 
     m_fSunShaftsIntensity = 0;
     m_fWaterIntensity = 1;
@@ -400,11 +400,11 @@ void CEnvDescriptor::load(CEnvironment& environment, const CInifile& config, pcs
     R_ASSERT(_valid(sun_dir));
 
     float degrees;
-    if (!config.read_if_exists(degrees, identifier, "sun_dir_azimuth"))
+    if (!config.read_if_exists(degrees, identifier, "sun_azimuth"))
         degrees = pSettingsOpenXRay->read_if_exists<float>("environment", "sun_dir_azimuth", 0.0f);
 
     clamp(degrees, 0.0f, 360.0f);
-    sun_dir_azimuth = deg2rad(degrees);
+    sun_azimuth = deg2rad(degrees);
 
     if (oldStyle)
     {
@@ -493,8 +493,8 @@ void CEnvDescriptor::save(CInifile& config, bool oldStyle /*= false*/) const
     {
         config.w_float(identifier, "sun_altitude",              rad2deg(sun_dir.getH()));
         config.w_float(identifier, "sun_longitude",             rad2deg(sun_dir.getP()));
+        config.w_float(identifier, "sun_azimuth",               sun_azimuth);
     }
-    config.w_float    (identifier, "sun_dir_azimuth",           sun_dir_azimuth);
     config.w_float    (identifier, "sun_shafts_intensity",      m_fSunShaftsIntensity);
 
     config.w_string   (identifier, thunderbolt_collection_name, tb_id.c_str());
@@ -521,7 +521,7 @@ void CEnvDescriptor::on_device_destroy()
 // Environment Mixer
 //-----------------------------------------------------------------------------
 CEnvDescriptorMixer::CEnvDescriptorMixer(shared_str const& identifier)
-    : CEnvDescriptor(identifier)
+    : CEnvDescriptor(identifier), soc_style(false)
 {
     use_dynamic_sun_dir = pSettingsOpenXRay->read_if_exists<bool>("environment", "dynamic_sun_dir", true);
 }
@@ -544,8 +544,7 @@ void CEnvDescriptorMixer::lerp(CEnvironment& parent, CEnvDescriptor& A, CEnvDesc
 {
     const float fi = 1 - f;
 
-    // XXX: it would be nice to lerp this too.
-    old_style = A.old_style;
+    exec_time = fi * A.exec_time + f * B.exec_time;
 
     m_pDescriptorMixer->lerp(&*A.m_pDescriptor, &*B.m_pDescriptor);
 
@@ -627,10 +626,10 @@ void CEnvDescriptorMixer::lerp(CEnvironment& parent, CEnvDescriptor& A, CEnvDesc
     sun_color.lerp(A.sun_color, B.sun_color, f);
 
      // Igor. Dynamic sun position.
-    if (!GEnv.Render->is_sun_static() && use_dynamic_sun_dir && !old_style)
+    if (!GEnv.Render->is_sun_static() && use_dynamic_sun_dir && !soc_style)
     {
-        sun_dir_azimuth = (fi * A.sun_dir_azimuth + f * B.sun_dir_azimuth);
-        calculate_dynamic_sun_dir(parent.GetGameTime());
+        sun_azimuth = (fi * A.sun_azimuth + f * B.sun_azimuth);
+        calculate_dynamic_sun_dir(exec_time);
     }
     else
     {
@@ -645,7 +644,7 @@ void CEnvDescriptorMixer::lerp(CEnvironment& parent, CEnvDescriptor& A, CEnvDesc
     tb_id = f < 0.5f ? A.tb_id : B.tb_id;
     env_ambient = Random.randF() < 1.f - f ? A.env_ambient : B.env_ambient;
 
-    if (old_style)
+    if (soc_style)
     {
         env_color =
         {
@@ -662,6 +661,9 @@ void CEnvDescriptorMixer::lerp(CEnvironment& parent, CEnvDescriptor& A, CEnvDesc
         };
     }
 
+    string_path temp_name;
+    sky_texture_name = strconcat(temp_name, A.sky_texture_name.c_str(), "; ", B.sky_texture_name.c_str());
+    clouds_texture_name = strconcat(temp_name, A.clouds_texture_name.c_str(), "; ", B.clouds_texture_name.c_str());
 }
 
 void CEnvDescriptorMixer::calculate_dynamic_sun_dir(float fGameTime)
@@ -710,7 +712,7 @@ void CEnvDescriptorMixer::calculate_dynamic_sun_dir(float fGameTime)
         cosAZ = (_sin(deg2rad(D)) - _sin(LatitudeR) * _cos(SZA)) / sin_SZA_X_cos_Latitude;
 
     clamp(cosAZ, -1.0f, 1.0f);
-    float AZ = acosf(cosAZ) + sun_dir_azimuth;
+    float AZ = acosf(cosAZ) + sun_azimuth;
 
     const Fvector2 minAngle = Fvector2().set(deg2rad(1.0f), deg2rad(3.0f));
 
@@ -883,14 +885,14 @@ void CEnvironment::load_weathers()
 
             EnvVec& env = WeatherCycles[weatherName];
             env.reserve(envCount);
-            
+            env.soc_style = true;
+
             pcstr executionTime, envSection;
             for (u32 envIdx = 0; envIdx < envCount; ++envIdx)
             {
                 if (pSettings->r_line(weatherSection, envIdx, &executionTime, &envSection))
                 {
                     env.emplace_back(create_descriptor(executionTime, pSettings, envSection));
-                    env.back()->old_style = true;
                 }
             }
         }
@@ -958,6 +960,7 @@ void CEnvironment::load_weather_effects()
         if (pSettings->r_line("weather_effects", weatherIdx, &weatherName, &weatherSection))
         {
             EnvVec& env = WeatherFXs[weatherName];
+            env.soc_style = true;
             env.emplace_back(create_descriptor("00:00:00", nullptr));
 
             const u32 envCount = pSettings->line_count(weatherSection);
@@ -967,7 +970,6 @@ void CEnvironment::load_weather_effects()
                 if (pSettings->r_line(weatherSection, envIdx, &executionTime, &envSection))
                 {
                     env.emplace_back(create_descriptor(executionTime, pSettings, envSection));
-                    env.back()->old_style = true;
                 }
             }
 
@@ -1035,51 +1037,59 @@ void CEnvironment::unload()
     m_pRender->OnUnload();
 }
 
-void CEnvironment::save(bool oldStyle /*= false*/) const
+void CEnvironment::save() const
 {
-    save_weathers(oldStyle);
-    save_weather_effects(oldStyle);
+    save_weathers();
+    save_weather_effects();
 }
 
-void CEnvironment::save_weathers(bool oldStyle /*= false*/) const
+void CEnvironment::save_weathers() const
 {
     string_path weathers_path;
-    if (oldStyle)
-        FS.update_path(weathers_path, "$game_config$", "weathers");
-    else
-        FS.update_path(weathers_path, "$game_weathers$", "");
+    if (!FS.update_path(weathers_path, "$game_weathers$", "", false))
+        FS.update_path(weathers_path, "$game_config$", "environment\\weathers");
+
+    string_path weathers_path_soc;
+    FS.update_path(weathers_path_soc, "$game_config$", "weathers\\");
 
     for (const auto& [name, descriptors] : WeatherCycles)
     {
+        const bool soc_style = descriptors.soc_style;
+
         string_path file_name;
-        strconcat(file_name, weathers_path, name.c_str(), ".ltx");
+        strconcat(file_name, soc_style ? weathers_path_soc : weathers_path,
+            name.c_str(), ".ltx");
 
         CInifile* config = xr_new<CInifile>(file_name, false, false, true);
 
         for (const auto& desc : descriptors)
-            desc->save(*config, oldStyle);
+            desc->save(*config, soc_style);
 
         CInifile::Destroy(config);
     }
 }
 
-void CEnvironment::save_weather_effects(bool oldStyle /*= false*/) const
+void CEnvironment::save_weather_effects() const
 {
     string_path effects_path;
-    if (oldStyle)
-        FS.update_path(effects_path, "$game_config$", "weathers");
-    else
-        FS.update_path(effects_path, "$game_weather_effects$", "");
+    if (!FS.update_path(effects_path, "$game_weather_effects$", "", false))
+        FS.update_path(effects_path, "$game_config$", "environment\\weather_effects");
+
+    string_path effects_path_soc;
+    FS.update_path(effects_path_soc, "$game_config$", "weathers\\");
 
     for (const auto& [name, descriptors] : WeatherFXs)
     {
+        const bool soc_style = descriptors.soc_style;
+
         string_path file_name;
-        strconcat(file_name, effects_path, name.c_str(), ".ltx");
+        strconcat(file_name, soc_style ? effects_path_soc : effects_path,
+            name.c_str(), ".ltx");
 
         CInifile* config = xr_new<CInifile>(file_name, false, false, true);
 
         for (const auto& desc : descriptors)
-            desc->save(*config, oldStyle);
+            desc->save(*config, soc_style);
 
         CInifile::Destroy(config);
     }

src/editors/xrWeatherEngine/engine_impl.cpp
@@ -346,9 +346,8 @@ void engine_impl::weather_current_time(char const* time)
         float(hours * 60 * 60 + minutes * 60 + seconds), environment.fTimeFactor);
     environment.m_paused = paused;
 
-    float weight;
     environment.Invalidate();
-    environment.lerp(weight);
+    environment.lerp();
 }
 
 void engine_impl::reload_current_time_frame()

src/xrEngine/Environment.cpp
@@ -227,6 +227,18 @@ void CEnvironment::SetGameTime(float game_time, float time_factor)
     fTimeFactor = time_factor;
 }
 
+void CEnvironment::SplitTime(float time, u32& hours, u32& minutes, u32& seconds) const
+{
+    u32 current_time_u32 = iFloor(time);
+    current_time_u32 = current_time_u32 % (24 * 60 * 60);
+
+    hours = current_time_u32 / (60 * 60);
+    current_time_u32 %= (60 * 60);
+
+    minutes = current_time_u32 / 60;
+    seconds = current_time_u32 % 60;
+}
+
 float CEnvironment::NormalizeTime(float tm)
 {
     if (tm < 0.f)
@@ -260,6 +272,7 @@ void CEnvironment::SetWeather(shared_str name, bool forced)
         {
             CurrentWeather = &it->second;
             CurrentWeatherName = it->first;
+            CurrentEnv->soc_style = CurrentWeather->soc_style;
         }
         if (forced)
         {
@@ -289,6 +302,7 @@ bool CEnvironment::SetWeatherFX(shared_str name)
         VERIFY(PrevWeather);
         CurrentWeather = &it->second;
         CurrentWeatherName = it->first;
+        CurrentEnv->soc_style = CurrentWeather->soc_style;
 
         float rewind_tm = WFX_TRANS_TIME * fTimeFactor;
         float start_tm = fGameTime + rewind_tm;

src/xrEngine/Environment.h
@@ -130,7 +130,6 @@ public:
 class ENGINE_API CEnvDescriptor
 {
 public:
-    bool old_style;
     bool dont_save; // oh
 
     float exec_time;
@@ -166,7 +165,8 @@ public:
     Fvector4 hemi_color; // w = R2 correction
     Fvector3 sun_color;
     Fvector3 sun_dir;
-    float sun_dir_azimuth; // for dynamic sun dir
+    float sun_azimuth; // for dynamic sun dir
+    bool use_dynamic_sun_dir;
 
     float m_fSunShaftsIntensity;
     float m_fWaterIntensity;
@@ -208,7 +208,7 @@ public:
     float fog_far;
     Fvector4 env_color;
 
-    bool use_dynamic_sun_dir;
+    bool soc_style;
 
 public:
     CEnvDescriptorMixer(shared_str const& identifier);
@@ -228,9 +228,12 @@ class ENGINE_API CEnvironment
     };
 
 public:
-    using EnvAmbVec = xr_vector<CEnvAmbient*>;
-    using EnvVec = xr_vector<CEnvDescriptor*>;
+    struct EnvVec : xr_vector<CEnvDescriptor*>
+    {
+        bool soc_style{};
+    };
     using EnvsMap = xr_map<shared_str, EnvVec, str_pred>;
+    using EnvAmbVec = xr_vector<CEnvAmbient*>;
 
 private:
     // clouds
@@ -307,7 +310,7 @@ public:
 
     virtual void load();
     virtual void unload();
-    void save(bool oldStyle = false) const;
+    void save() const;
 
     void mods_load();
     void mods_unload();
@@ -347,6 +350,12 @@ public:
 
     bool m_paused;
 #endif // #ifdef _EDITOR
+    void GetGameTime(u32& hours, u32& minutes, u32& seconds) const
+    {
+        SplitTime(fGameTime, hours, minutes, seconds);
+    }
+
+    void SplitTime(float time, u32& hours, u32& minutes, u32& seconds) const;
 
     CInifile* m_ambients_config;
     CInifile* m_sound_channels_config;
@@ -364,8 +373,8 @@ protected:
 
     void load_level_specific_ambients();
 
-    void save_weathers(bool oldStyle = false) const;
-    void save_weather_effects(bool oldStyle = false) const;
+    void save_weathers() const;
+    void save_weather_effects() const;
 
 public:
     virtual SThunderboltDesc* thunderbolt_description(const CInifile& config, shared_str const& section);

src/xrEngine/editor_weather.cpp
@@ -9,10 +9,14 @@
 
 namespace xray::editor
 {
-static void ItemHelp(const char* desc, bool use_separate_marker = true)
+static void ItemHelp(const char* desc, bool use_separate_marker = true, bool call_same_line = true)
 {
     if (use_separate_marker)
+    {
+        if (call_same_line)
+            ImGui::SameLine();
         ImGui::TextDisabled("(?)");
+    }
     if (ImGui::IsItemHovered(ImGuiHoveredFlags_DelayShort))
     {
         ImGui::BeginTooltip();
@@ -38,6 +42,68 @@ bool TextureSelector(pcstr label, shared_str& texture_name)
     return changed;
 }
 
+struct combo_raii
+{
+    ~combo_raii()
+    {
+        ImGui::EndCombo();
+    }
+};
+
+bool TimeFrameCombo(pcstr label, CEnvDescriptor*& descriptor)
+{
+    const auto& env = g_pGamePersistent->Environment();
+
+    if (ImGui::BeginCombo(label, descriptor ? descriptor->m_identifier.c_str() : "##"))
+    {
+        combo_raii raii;
+
+        if (ImGui::Selectable("##", !descriptor))
+        {
+            descriptor = nullptr;
+            return true;
+        }
+        for (const auto& desc : *env.CurrentWeather)
+        {
+            if (ImGui::Selectable(desc->m_identifier.c_str(), descriptor ? descriptor->m_identifier == desc->m_identifier : false))
+            {
+                descriptor = desc;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool ConfigStyleSelector(pcstr label, bool& soc_style)
+{
+    bool result = false;
+    enum ConfigStyle : int
+    {
+        ConfigStyle_SOC,
+        ConfigStyle_CSCOP,
+        ConfigStyle_COUNT
+    };
+    constexpr pcstr styles[ConfigStyle_COUNT] =
+    {
+        "SOC",
+        "CS/COP"
+    };
+    int selected = soc_style ? ConfigStyle_SOC : ConfigStyle_CSCOP;
+    if (ImGui::SliderInt(label, &selected, 0, ConfigStyle_COUNT - 1, styles[selected]))
+    {
+        switch ((ConfigStyle)selected)
+        {
+        case ConfigStyle_SOC:   soc_style = true; break;
+        case ConfigStyle_CSCOP: soc_style = false; break;
+        }
+        result = true;
+    }
+    ItemHelp("This affects how environment color will be calculated in the mixer. "
+             "(look at the environment color in the mixer category)");
+    return result;
+}
+
 template <typename T>
 void display_property(T&) {}
 
@@ -47,34 +113,6 @@ void display_property(CEnvDescriptor& descriptor)
     auto& env = g_pGamePersistent->Environment();
 
     ImGui::PushID(descriptor.m_identifier.c_str());
-
-    ImGui::LabelText("identifier", "%s", descriptor.m_identifier.c_str());
-    {
-        enum ConfigStyle : int
-        {
-            ConfigStyle_SOC,
-            ConfigStyle_CSCOP,
-            ConfigStyle_COUNT
-        };
-        constexpr pcstr styles[ConfigStyle_COUNT] =
-        {
-            "SOC",
-            "CS/COP"
-        };
-        int selected = descriptor.old_style ? ConfigStyle_SOC : ConfigStyle_CSCOP;
-        if (ImGui::SliderInt("config style", &selected, 0, ConfigStyle_COUNT - 1, styles[selected]))
-        {
-            switch ((ConfigStyle)selected)
-            {
-                case ConfigStyle_SOC:   descriptor.old_style = true; break;
-                case ConfigStyle_CSCOP: descriptor.old_style = false; break;
-            }
-        }
-        ImGui::SameLine();
-        ItemHelp("This affects how environment color will be calculated in the mixer. "
-                 "(look for details in the environment color help marker in the mixer category)");
-    }
-
     if (ImGui::CollapsingHeader("sun##category", ImGuiTreeNodeFlags_DefaultOpen))
     {
         if (ImGui::BeginCombo("sun##lensflareid", descriptor.lens_flare_id.c_str()))
@@ -88,30 +126,30 @@ void display_property(CEnvDescriptor& descriptor)
             }
             ImGui::EndCombo();
         }
-        ImGui::SameLine();
         ItemHelp("Name in configs: \n"
-            "CS/COP: sun\n"
-            "   SOC: flares");
+                 "CS/COP: sun\n"
+                 "   SOC: flares");
 
         ImGui::ColorEdit3("sun color", (float*)&descriptor.sun_color);
 
-        bool result = false;
-        float y, x;
-        descriptor.sun_dir.getHP(y, x);
-        result  = ImGui::DragFloat("altitude", &y, 0.5f, -360.0f, 360.f);
-        result |= ImGui::DragFloat("longitude", &x, 0.5f, -360.0f, 360.f);
-        if (result)
-        {
-            descriptor.sun_dir.setHP(deg2rad(y), deg2rad(x));
-        }
+        ImGui::Checkbox("Dynamic sun direction", &descriptor.use_dynamic_sun_dir);
 
-        float azimuth = rad2deg(descriptor.sun_dir_azimuth);
+        float altitude  = rad2deg(descriptor.sun_dir.getH());
+        float longitude = rad2deg(descriptor.sun_dir.getP());
+
+        if (ImGui::DragFloat("altitude", &altitude, 0.5f, -360.0f, 360.f))
+            descriptor.sun_dir.setHP(deg2rad(altitude), deg2rad(longitude));
+        if (ImGui::DragFloat("longitude", &longitude, 0.5f, -360.0f, 360.f))
+            descriptor.sun_dir.setHP(deg2rad(altitude), deg2rad(longitude));
+
+        ImGui::BeginDisabled(!descriptor.use_dynamic_sun_dir);
+        float azimuth = rad2deg(descriptor.sun_azimuth);
         if (ImGui::DragFloat("azimuth", &azimuth, 0.5f, -360.0f, 360.f))
-            descriptor.sun_dir_azimuth = deg2rad(azimuth);
-        ImGui::SameLine();
+            descriptor.sun_azimuth = deg2rad(azimuth);
+        ImGui::EndDisabled();
         ItemHelp("Dynamic sun dir azimuth correction.\n"
                  "Name in configs: \n"
-                 "sun_dir_azimuth");
+                 "sun_azimuth");
 
         ImGui::DragFloat("shafts intensity", &descriptor.m_fSunShaftsIntensity, 0.001f, 0.0f, 1.0f);
     }
@@ -128,7 +166,6 @@ void display_property(CEnvDescriptor& descriptor)
 
         ImGui::ColorEdit3("sky color", (float*)&descriptor.sky_color);
         ImGui::ColorEdit4("hemi color", (float*)&descriptor.hemi_color, ImGuiColorEditFlags_AlphaBar);
-        ImGui::SameLine();
         ItemHelp("Name in configs: \n"
                  "hemisphere_color");
 
@@ -159,13 +196,11 @@ void display_property(CEnvDescriptor& descriptor)
             }
             ImGui::EndCombo();
         }
-        ImGui::SameLine();
         ItemHelp("Name in configs: \n"
-            "CS/COP: ambient\n"
-            "   SOC: env_ambient");
+                 "CS/COP: ambient\n"
+                 "   SOC: env_ambient");
 
         ImGui::ColorEdit3("ambient color", (float*)&descriptor.ambient);
-        ImGui::SameLine();
         ItemHelp("Name in configs: \n"
                  "CS/COP: ambient_color\n"
                  "   SOC: ambient");
@@ -175,6 +210,7 @@ void display_property(CEnvDescriptor& descriptor)
         ImGui::DragFloat("far plane", &descriptor.far_plane);
         ImGui::ColorEdit3("fog color", (float*)&descriptor.fog_color);
         ImGui::DragFloat("fog distance", &descriptor.fog_distance, 1.0f, 0.0f, descriptor.far_plane);
+        ItemHelp("Fog distance can't be higher than far plane.");
         ImGui::DragFloat("fog density", &descriptor.fog_density, 0.001f, 0.0f, 1.0f);
         ImGui::DragFloat("water intensity", &descriptor.m_fWaterIntensity, 0.001f, 0.0f, 1.0f);
     }
@@ -196,10 +232,9 @@ void display_property(CEnvDescriptor& descriptor)
             }
             ImGui::EndCombo();
         }
-        ImGui::SameLine();
         ItemHelp("Name in configs: \n"
-            "CS/COP: thunderbolts_collection\n"
-            "   SOC: thunderbolt");
+                 "CS/COP: thunderbolts_collection\n"
+                 "   SOC: thunderbolt");
 
         ImGui::DragFloat("duration", &descriptor.bolt_duration);
         ImGui::DragFloat("period", &descriptor.bolt_period);
@@ -225,7 +260,6 @@ void display_property(CEnvDescriptorMixer& descriptor)
     if (ImGui::CollapsingHeader("mixer##category", ImGuiTreeNodeFlags_DefaultOpen))
     {
         ImGui::ColorEdit4("environment color", (float*)&descriptor.env_color);
-        ImGui::SameLine();
         ItemHelp("CS/COP: takes hemisphere color as the base value.\n"
                  "SOC   : takes sky color as the base value.");
 
@@ -242,62 +276,125 @@ void ide::ShowWeatherEditor()
     if (ImGui::Begin("Weather editor", &m_windows.weather, get_default_window_flags()))
     {
         auto& env = g_pGamePersistent->Environment();
-
-        static bool force_change_weather = true;
-        ImGui::Checkbox("##", &force_change_weather);
-        const auto prev_size = ImGui::GetItemRectSize();
-        ImGui::SameLine();
-        ItemHelp("Force change weather", false);
-        ImGui::SameLine();
-
-        //ImGui::SetNextItemWidth(ImGui::GetContentRegionAvail().x - prev_size.x);
-        if (ImGui::BeginCombo("current weather cycle", env.CurrentCycleName.c_str()))
-        {
-            for (const auto& [identifier, env_descriptors] : env.WeatherCycles)
-            {
-                if (ImGui::Selectable(identifier.c_str(), env.CurrentCycleName == identifier))
-                    env.SetWeather(identifier, force_change_weather);
-            }
-            ImGui::EndCombo();
-        }
-
-        if (ImGui::BeginCombo("current weather fx cycle", env.bWFX ? env.CurrentWeatherName.c_str() : ""))
-        {
-            if (ImGui::Selectable("##", !env.bWFX))
-                env.StopWFX();
-
-            for (const auto& [identifier, env_descriptors] : env.WeatherFXs)
-            {
-                if (ImGui::Selectable(identifier.c_str(), env.CurrentWeatherName == identifier))
-                {
-                    if (env.bWFX)
-                        env.StopWFX();
-                    env.SetWeatherFX(identifier);
-                }
-            }
-            ImGui::EndCombo();
-        }
-
         auto& current = *env.CurrentEnv;
         auto& current0 = env.Current[0] ? *env.Current[0] : *env.CurrentEnv;
         auto& current1 = env.Current[1] ? *env.Current[1] : *env.CurrentEnv;
+        float time_factor = env.fTimeFactor;
 
-        if (ImGui::BeginCombo("current time frame", current0.m_identifier.c_str()))
+        if (ImGui::Button("Reset all"))
+            ;
+        ImGui::SameLine();
+        if (ImGui::Button("Save all"))
+            env.save();
+
+        if (ImGui::CollapsingHeader("Environment time", ImGuiTreeNodeFlags_DefaultOpen))
         {
-            for (const auto& descriptor : *env.CurrentWeather)
+            float time = env.GetGameTime();
+
+            u32 hours, minutes, seconds;
+            env.SplitTime(time, hours, minutes, seconds);
+
+            string128 temp;
+            xr_sprintf(temp, "Current time: %02d:%02d:%02d###environment_time", hours, minutes, seconds);
+
+            if (ImGui::SliderFloat(temp, &time, 0, DAY_LENGTH, "", ImGuiSliderFlags_NoInput))
             {
-                if (ImGui::Selectable(descriptor->m_identifier.c_str(), descriptor->m_identifier == current0.m_identifier))
-                    ;
+                env.SetGameTime(time, time_factor);
+                env.Invalidate();
+                env.lerp();
             }
-            ImGui::EndCombo();
+            if (ImGui::DragFloat("Time factor", &time_factor, 1.f, 1.f, 1000.f))
+            {
+                env.SetGameTime(time, time_factor);
+            }
         }
-        if (ImGui::Button("Save"))
-            env.save(current.old_style);
+
+        if (ImGui::CollapsingHeader("Current environment##header", ImGuiTreeNodeFlags_DefaultOpen))
+        {
+            if (ImGui::BeginTable("Current environment##table", 2))
+            {
+                ImGui::TableNextColumn();
+                static bool force_change_weather = true;
+                if (ImGui::Checkbox("##force_change_weather", &force_change_weather))
+                {
+                    if (force_change_weather)
+                        env.SetWeather(env.CurrentCycleName, true);
+                }
+                ItemHelp("Force change weather", false);
+                ImGui::SameLine();
+
+                if (ImGui::BeginCombo("Weather cycle", env.CurrentCycleName.c_str()))
+                {
+                    for (const auto& [identifier, env_descriptors] : env.WeatherCycles)
+                    {
+                        if (ImGui::Selectable(identifier.c_str(), env.CurrentCycleName == identifier))
+                            env.SetWeather(identifier, force_change_weather);
+                    }
+                    ImGui::EndCombo();
+                }
+                //ImGui::SameLine();
+                if (ConfigStyleSelector("Weather config style", env.CurrentWeather->soc_style))
+                    current.soc_style = env.CurrentWeather->soc_style;
+
+                ImGui::TableNextColumn();
+                if (ImGui::BeginCombo("Effect", env.IsWFXPlaying() ? env.CurrentWeatherName.c_str() : ""))
+                {
+                    if (ImGui::Selectable("##", !env.IsWFXPlaying()))
+                        env.StopWFX();
+
+                    for (const auto& [identifier, env_descriptors] : env.WeatherFXs)
+                    {
+                        if (ImGui::Selectable(identifier.c_str(), env.CurrentWeatherName == identifier))
+                        {
+                            if (env.IsWFXPlaying())
+                                env.StopWFX();
+                            env.SetWeatherFX(identifier);
+                        }
+                    }
+                    ImGui::EndCombo();
+                }
+                //ImGui::SameLine();
+                ImGui::BeginDisabled(!env.IsWFXPlaying());
+                if (ConfigStyleSelector("Effect config style", env.CurrentWeather->soc_style))
+                    current.soc_style = env.CurrentWeather->soc_style;
+                ImGui::EndDisabled();
+                ImGui::EndTable();
+            }
+        }
+
+        ImGui::Spacing();
 
         constexpr ImGuiTableFlags flags = ImGuiTableFlags_Resizable | ImGuiTableFlags_Reorderable |
             ImGuiTableFlags_Hideable | ImGuiTableFlags_BordersInner;
 
-        if (ImGui::BeginTable("time lerp", 3, flags))
+        if (ImGui::BeginTable("Environment lerp", 3, flags))
+        {
+            ImGui::TableNextColumn();
+            TimeFrameCombo("Time frame##current0", env.Current[0]);
+
+            ImGui::TableNextColumn();
+            u32 hours, minutes, seconds;
+            env.SplitTime(current.exec_time, hours, minutes, seconds);
+
+            string128 temp;
+            xr_sprintf(temp, "%02d:%02d:%02d###frame_time", hours, minutes, seconds);
+
+            ImGui::BeginDisabled(!env.Current[0] || !env.Current[1]);
+            if (ImGui::SliderFloat(temp, &current.exec_time,
+                current0.exec_time, current1.exec_time, "", ImGuiSliderFlags_NoInput))
+            {
+                env.SetGameTime(current.exec_time, time_factor);
+                env.lerp();
+            }
+            ImGui::EndDisabled();
+
+            ImGui::TableNextColumn();
+            TimeFrameCombo("Time frame##current1", env.Current[1]);
+
+            ImGui::EndTable();
+        }
+
+        if (ImGui::BeginTable("Environment lerp", 3, flags))
         {
             ImGui::TableSetupColumn("current");
             ImGui::TableSetupColumn("blend");
@@ -305,11 +402,20 @@ void ide::ShowWeatherEditor()
             ImGui::TableHeadersRow();
 
             ImGui::TableNextColumn();
-            display_property(current0);
+            if (env.Current[0])
+                display_property(current0);
+            else
+                ImGui::Text("Please, load a level or select time frame manually.");
+
             ImGui::TableNextColumn();
             display_property(current);
+
             ImGui::TableNextColumn();
-            display_property(current1);
+            if (env.Current[1])
+                display_property(current1);
+            else
+                ImGui::Text("Please, load a level or select time frame manually.");
+
             ImGui::EndTable();
         }
     }

chronic8000/OpenXRay-Pi5

commit 1b5b724898d9b752f73bc1bf050f1f93cfbaa772

Fixed crash when clouds_texture is empty (fixes #1821)
Was introduced in f9543167cb4666471d45a7fa2b905cd6d128e0d4

src/xrEngine/Environment_misc.cpp
@@ -651,8 +651,16 @@ void CEnvDescriptorMixer::lerp(CEnvironment& parent, CEnvDescriptor& A, CEnvDesc
     }
 
     string_path temp_name;
-    sky_texture_name = strconcat(temp_name, A.sky_texture_name.c_str(), "; ", B.sky_texture_name.c_str());
-    clouds_texture_name = strconcat(temp_name, A.clouds_texture_name.c_str(), "; ", B.clouds_texture_name.c_str());
+
+    if (!A.sky_texture_name.empty() && !B.sky_texture_name.empty())
+        sky_texture_name = strconcat(temp_name, A.sky_texture_name.c_str(), "; ", B.sky_texture_name.c_str());
+    else
+        sky_texture_name = f < 0.5f ? A.sky_texture_name : B.sky_texture_name;
+
+    if (!A.clouds_texture_name.empty() && !B.clouds_texture_name.empty())
+        clouds_texture_name = strconcat(temp_name, A.clouds_texture_name.c_str(), "; ", B.clouds_texture_name.c_str());
+    else
+        clouds_texture_name = f < 0.5f ? A.clouds_texture_name : B.clouds_texture_name;
 }
 
 std::pair<Fvector3, float> CEnvDescriptorMixer::calculate_dynamic_sun_dir(float fGameTime, float azimuth)