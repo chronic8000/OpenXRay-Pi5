chronic8000/OpenXRay-Pi5

commit 38c61b1639e1c4c87c0a13c98e85da0ad5c51b11

xrEngine/EngineAPI.cpp: no longer need to copy renderer mode strings
Libraries aren't unloaded anymore after
f351ad4cc6e8d3201af2df1c56ac7cc781f617e0

src/xrEngine/EngineAPI.cpp
@@ -9,7 +9,6 @@
 #include "XR_IOConsole.h"
 
 #include "xrCore/xr_token.h"
-#include "xrCore/ModuleLookup.hpp"
 
 #include "xrScriptEngine/ScriptExporter.hpp"
 
@@ -72,7 +71,7 @@ void CEngineAPI::SelectRenderer()
             if (renderer->CheckGameRequirements())
             {
                 selectedRenderer = renderer;
-                selected_mode = mode.c_str();
+                selected_mode = mode;
                 string64 buf;
                 xr_sprintf(buf, "renderer %s", selected_mode);
                 Console->Execute(buf);
@@ -143,10 +142,8 @@ void CEngineAPI::CreateRendererList(const std::array<RendererModule*, 2>& module
                 VERIFY3(false, "Renderer mode duplicate. Skipping.", mode);
                 continue;
             }
-            // mode string will be freed after library unloading, copy.
-            shared_str copiedMode = mode;
             renderModes[mode] = module;
-            VidQualityToken.emplace_back(copiedMode.c_str(), modeIndex);
+            VidQualityToken.emplace_back(mode, modeIndex);
         }
 
         return true;

src/xrEngine/EngineAPI.h
@@ -62,7 +62,7 @@ public:
 
 class ENGINE_API CEngineAPI
 {
-    xr_map<shared_str, RendererModule*> renderModes;
+    xr_map<cpcstr, RendererModule*> renderModes;
 
     GameModule* gameModule{};
     RendererModule* selectedRenderer{};

commit c4582fa4f26fb6750a098021f918d8d0f9965ac9

Fix user selected renderer initialization
The fixes a bug introduced in 38c61b1639e1c4c87c0a13c98e85da0ad5c51b11

src/xrEngine/EngineAPI.cpp
@@ -51,10 +51,14 @@ void CEngineAPI::SelectRenderer()
 {
     ZoneScoped;
 
-    // User has some renderer selected
-    // Find it and check if we can use it
+    // User has some renderer selected, find it
     pcstr selected_mode = Console->GetString("renderer");
-    const auto it = renderModes.find(selected_mode);
+    const auto it = std::find_if(renderModes.begin(), renderModes.end(), [selected_mode](const auto& pair)
+    {
+        return xr_strcmp(selected_mode, pair.first) == 0;
+    });
+
+    // Check if we can use it
     if (it != renderModes.end())
     {
         if (it->second->CheckGameRequirements())
@@ -62,7 +66,7 @@ void CEngineAPI::SelectRenderer()
     }
 
     // Renderer is either fully unsupported (hardware)
-    // or we don't comply with it requirements (e.g. shaders missing)
+    // or we don't comply with it's requirements (e.g. shaders missing)
     if (!selectedRenderer)
     {
         // Select any suitable
@@ -134,7 +138,7 @@ void CEngineAPI::CreateRendererList(const std::array<RendererModule*, 2>& module
         if (modes.empty())
             return false;
 
-        for (auto [mode, modeIndex] : modes)
+        for (const auto [mode, modeIndex] : modes)
         {
             const auto it = renderModes.find(mode);
             if (it != renderModes.end())

src/xrEngine/xr_ioc_cmd.cpp
@@ -644,8 +644,6 @@ public:
             return;
         }
 
-        tokens = VidQualityToken.data();
-
         inherited::Execute(args);
 
         cmd_lock = true;