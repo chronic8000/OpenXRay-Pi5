OpenXRay/xray-16

commit 8d6e32ed3edce351cb3c501fc913d8ee7f7bc72b

xrGame/visual_memory_manager.cpp: load vision_free_section or default section (#382)

src/xrGame/visual_memory_manager.cpp
@@ -149,8 +149,8 @@ void CVisualMemoryManager::reload(LPCSTR section)
     }
     else if (m_object)
     {
-        m_free.Load(pSettings->r_string(section, "vision_free_section"), !!m_client);
-        m_danger.Load(pSettings->r_string(section, "vision_danger_section"), !!m_client);
+        m_free.Load(READ_IF_EXISTS(pSettings, r_string, section, "vision_free_section", section), !!m_client);
+        m_danger.Load(READ_IF_EXISTS(pSettings, r_string, section, "vision_free_section", section), !!m_client);
     }
     else
     {

chronic8000/OpenXRay-Pi5

commit 9552f021e337ca5e370a208f1b4e99a4f251566d

xrGame/visual_memory_manager.cpp: load vision_danger_section correctly
This fixes bug introduced in 8d6e32ed3edce351cb3c501fc913d8ee7f7bc72b

src/xrGame/visual_memory_manager.cpp
@@ -152,7 +152,7 @@ void CVisualMemoryManager::reload(LPCSTR section)
     else if (m_object)
     {
         m_free.Load(READ_IF_EXISTS(pSettings, r_string, section, "vision_free_section", section), !!m_client);
-        m_danger.Load(READ_IF_EXISTS(pSettings, r_string, section, "vision_free_section", section), !!m_client);
+        m_danger.Load(READ_IF_EXISTS(pSettings, r_string, section, "vision_danger_section", section), !!m_client);
     }
     else
     {