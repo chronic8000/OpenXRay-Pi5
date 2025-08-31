dc415c04bcb55d1d59834766dff51a58a464317d
Xottab-DUTY <xottab-duty@yandex.ru>
2025-08-31 21:54:41 +0300

    xrRender/R_Backend.h: constify set_Scissor
    Same change in xrEngine/Device_imgui.cpp

--- src/xrEngine/Device_imgui.cpp
+++ src/xrEngine/Device_imgui.cpp
@@ -69,12 +69,12 @@ void CRenderDevice::InitializeImGui()
     {
         if (data->WantVisible)
         {
-            /*const*/ SDL_Rect r // this is not const because older versions of SDL accept non-const rect
+            const SDL_Rect r
             {
-                /*.x =*/ (int)(data->InputPos.x - viewport->Pos.x),
-                /*.y =*/ (int)(data->InputPos.y - viewport->Pos.y + data->InputLineHeight),
-                /*.w =*/ 1,
-                /*.h =*/ (int)data->InputLineHeight,
+                .x = (int)(data->InputPos.x - viewport->Pos.x),
+                .y = (int)(data->InputPos.y - viewport->Pos.y + data->InputLineHeight),
+                .w = 1,
+                .h = (int)data->InputLineHeight,
             };
             SDL_SetTextInputRect(&r);
         }
