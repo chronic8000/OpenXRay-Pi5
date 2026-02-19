dab72348b268b4548e902dbf28118e5c0bcf83d1
chronic <chronic8000@gmail.com>
2026-02-19 18:18:20 +0000

    Final Silicon Whisperer release: Recursive archive fix, Launcher paths standardized, and Desktop entries synced

--- misc/openxray-wrapper.sh
+++ misc/openxray-wrapper.sh
@@ -2,12 +2,14 @@
 # Homeless Tech OpenXRay Launcher for Pi 5
 
 # 1. Apply runtime tweaks
-/usr/bin/rpi5_optimize.sh
+/usr/games/rpi5_optimize.sh
 
 # 2. V3D / Mesa Overrides
 export v3d_debug=no_vpm_wait
 export MESA_LOADER_DRIVER_OVERRIDE=zink
 
-# 3. Launch with taskset to isolate Core 0 (System) and utilize 1-3 for Game
-# We also use numactl to enforce interleaving for the process itself to be sure
+# 3. Execution Root
+cd /usr/share/stalker-pi5
+
+# 4. Launch with taskset and NUMA interleaving
 taskset -c 1-3 numactl --interleave=all /usr/games/xr_3da "$@"
